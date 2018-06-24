c8f281eeebc4ce81f75b6157ffca79020bc6addd
ushills <web@ushills.co.uk>
2018-06-24 11:00:02 +0100

    Added cars and associated test from https://codechalleng.es/bites/21/

---
+++ days/07-09-data-structures/code/cars.py
@@ -0,0 +1,40 @@
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps():
+    jeeps = cars['Jeep']
+    models = ', '.join(jeeps)
+    return models
+
+
+def get_first_model_each_manufacturer():
+    firstmodels = []
+    for maker in cars:
+        models = cars[maker]
+        firstmodels.append(models[0])
+    return firstmodels
+
+
+def get_all_matching_models(grep='trail'):
+    search_result = []
+    for models in cars.values():
+        for model in models:
+            if grep.lower() in model.lower():
+                search_result.append(model)
+    search_result.sort()
+    return search_result
+
+
+def sort_car_models():
+    new_cars = {}
+    for maker in cars:
+        models = cars[maker]
+        models.sort()
+        new_cars[maker] = models
+    return new_cars
