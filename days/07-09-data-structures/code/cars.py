76bbc10c808065980802987df703e7cee993e42d
ushills <web@ushills.co.uk>
2018-06-25 10:46:07 +0100

    Updated 07-09 with list and dictionary comprehensions

--- days/07-09-data-structures/code/cars.py
+++ days/07-09-data-structures/code/cars.py
@@ -6,35 +6,53 @@ cars = {
     'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
 }
 
+# def get_all_jeeps():
+#     jeeps = cars['Jeep']
+#     models = ', '.join(jeeps)
+#     return models
 
 def get_all_jeeps():
-    jeeps = cars['Jeep']
-    models = ', '.join(jeeps)
-    return models
+    return ', '.join(cars['Jeep'])
 
+
+# def get_first_model_each_manufacturer():
+#     firstmodels = []
+#     for maker in cars:
+#         models = cars[maker]
+#         firstmodels.append(models[0])
+#     return firstmodels
 
 def get_first_model_each_manufacturer():
-    firstmodels = []
-    for maker in cars:
-        models = cars[maker]
-        firstmodels.append(models[0])
-    return firstmodels
+    return [models[0] for models in cars.values()]
+
+
+# def get_all_matching_models(grep='trail'):
+#     search_result = []
+#     for models in cars.values():
+#         for model in models:
+#             if grep.lower() in model.lower():
+#                 search_result.append(model)
+#     search_result.sort()
+#     return search_result
 
 
 def get_all_matching_models(grep='trail'):
-    search_result = []
-    for models in cars.values():
-        for model in models:
-            if grep.lower() in model.lower():
-                search_result.append(model)
-    search_result.sort()
-    return search_result
+    grep = grep.lower()
+    models = sum(cars.values(), [])  # flatten list of lists
+    matching_models = [model for model in models
+                       if grep in model.lower()]
+    return sorted(matching_models)
 
+
+# def sort_car_models():
+#     new_cars = {}
+#     for maker in cars:
+#         models = cars[maker]
+#         models.sort()
+#         new_cars[maker] = models
+#     return new_cars
 
 def sort_car_models():
-    new_cars = {}
-    for maker in cars:
-        models = cars[maker]
-        models.sort()
-        new_cars[maker] = models
-    return new_cars
+    """sort the car models (values) and return the resulting cars dict"""
+    return {manufacturer: sorted(models) for
+            manufacturer, models in cars.items()}
